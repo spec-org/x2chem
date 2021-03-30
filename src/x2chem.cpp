7cabbb904d19fa91d6f5a60607e29a550db7aba6
Andrew Wildman <apw4@uw.edu>
2021-03-29 18:22:59 -0700

    Restructure project

--- src/x2chem.cpp
+++ src/x2chem.cpp
@@ -1,3 +1,5 @@
+#include <x2chem.hpp>
+
 void x2c_hamiltonian(int NB, double *S, double *T, double *V, double *pVp, double *pVxp, double *U, double *coreH) {
     for (auto i = 0; i < NB; i++) 
     for (auto j = 0; j < NB; j++) {
