f8a306b512fcca411f6912159239fe2cccb69dc5
Andrew Wildman <apw4@uw.edu>
2021-03-30 10:55:10 -0700

    Change API to be more succinct

--- include/x2chem.hpp
+++ include/x2chem.hpp
@@ -1,6 +1,28 @@
-// Compute X2C Core Hamiltonian
-void x2c_hamiltonian(int NB, double *S, double *T, double *V, double *pVp, 
-                     double *pVxp, double *U, double *coreH);
+#pragma once
 
-// Boettger 2e SOC scaling factor
-void boettger_2e_soc(double *basis, double *coreH);
+#include <array>
+#include <complex>
+
+namespace X2Chem {
+
+  // Inputs to x2c_hamiltonian
+  struct Integrals {
+    const std::complex<double>* S;                  ///< Overlap
+    const std::complex<double>* T;                  ///< Kinetic
+    const std::complex<double>* V;                  ///< Nuclear potential
+    const std::array<std::complex<double>*,4> pVp;  ///< Spin-orbit
+  };
+
+  // Operators output by x2c_hamiltonian
+  struct X2COperators {
+    std::complex<double>* U;     ///< Transformation matrix
+    std::complex<double>* coreH; ///< Core hamiltonian
+  };
+  
+  // Compute X2C Core Hamiltonian
+  void x2c_hamiltonian(const unsigned int, const Integrals&, X2COperators&);
+  
+  // Boettger 2e SOC scaling factor
+  void boettger_2e_soc(double*, double*);
+
+}
--- src/x2chem.cpp
+++ src/x2chem.cpp
@@ -1,13 +1,24 @@
 #include <x2chem.hpp>
 
-void x2c_hamiltonian(int NB, double *S, double *T, double *V, double *pVp, double *pVxp, double *U, double *coreH) {
-    for (auto i = 0; i < NB; i++) 
-    for (auto j = 0; j < NB; j++) {
-      U[i + j*NB] += 1.0;
-    }
-    return;
-}
+namespace X2Chem {
 
-void boettger_2e_soc(double *basis, double *coreH) {
+  void x2c_hamiltonian(const unsigned int nb, const Integrals& ints,
+    X2COperators& output)
+  {
+  
+    std::complex<double>* U = output.U;
+    
+    for (auto i = 0; i < nb; i++) 
+    for (auto j = 0; j < nb; j++) {
+      U[i + j*nb] += 1.0;
+    }
+  
     return;
-}
+  }
+  
+  void boettger_2e_soc(double *basis, double *coreH)
+  {
+    return;
+  }
+
+} // namespace X2Chem
