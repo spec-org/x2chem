72dc4be0b7086ee0b658838eb956c9408a9c3b10
Torin Stetina <torins@uw.edu>
2021-03-30 17:13:53 -0700

    added auxillary functions for building 4C Ham

--- include/x2chem.hpp
+++ include/x2chem.hpp
@@ -5,6 +5,9 @@
 
 namespace X2Chem {
 
+  // Physical constants
+  constexpr double LIGHTSPEED = 137.035999074; // Atomic units
+
   // Inputs to x2c_hamiltonian
   struct Integrals {
     const double* S;                  ///< Overlap
@@ -20,9 +23,17 @@ namespace X2Chem {
   };
   
   // Compute X2C Core Hamiltonian
-  void x2c_hamiltonian(const unsigned int, const Integrals&, X2COperators&);
+  void x2c_hamiltonian(const unsigned int, const Integrals&, X2COperators&, std::complex<double>*);
   
   // Boettger 2e SOC scaling factor
   void boettger_2e_soc(double*, double*);
+
+  // Construct 4C Core Hamiltonian
+  void _build_4c_core_ham(const unsigned int, const Integrals&, std::complex<double>*);
+
+
+  // Auxilary functions
+  void _real_submat_to_complex(unsigned int, unsigned int, const double*, unsigned int, 
+    std::complex<double>*, unsigned int); 
 
 }
--- src/x2chem.cpp
+++ src/x2chem.cpp
@@ -1,17 +1,50 @@
+#include <iostream>
+#include <lapack.hh>
 #include <x2chem.hpp>
 
 namespace X2Chem {
 
-  void x2c_hamiltonian(const unsigned int nb, const Integrals& ints,
-    X2COperators& output)
+  // Real part of A is written to arbitrary subblock of B
+  //
+  //                         [. . .]  
+  // real(A) -> complex(B) = [. A .]
+  //                         [. . .]
+  //
+  void _real_submat_to_complex(unsigned int n, unsigned int m, const double* A, unsigned int LDA, 
+    std::complex<double>* B, unsigned int LDB) 
+  {
+    for (auto i = 0; i < m; i++)
+    for (auto j = 0; j < n; j++) {
+        B[i*LDB + j] = std::complex<double>(A[i*LDA + j],0.0);
+    }
+  }
+
+  void _build_4c_core_ham(const unsigned int nb, const Integrals& ints, std::complex<double>* core4c)
   {
   
+    // Set spatial ints to subblocks of 4C
+    _real_submat_to_complex(nb, nb, ints.S, nb, core4c, 4*nb);
+
+    for (auto i = 0; i < 4*nb; i++) 
+    for (auto j = 0; j < 4*nb; j++) {
+      std::cout << "4C[" << i << "," << j << "]: " << core4c[i*4*nb + j] << std::endl;
+    }
+
+    
+
+    return;
+  }
+
+  void x2c_hamiltonian(const unsigned int nb, const Integrals& ints,
+    X2COperators& output, std::complex<double>* core4c)
+  {
+  
+    //const double* S = ints.S;
     std::complex<double>* U = output.U;
     
-    for (auto i = 0; i < nb; i++) 
-    for (auto j = 0; j < nb; j++) {
-      U[i*nb + j] += 1.0;
-    }
+    // Build 4C Core Hamiltonian
+    _build_4c_core_ham(nb, ints, core4c);
+
   
     return;
   }
