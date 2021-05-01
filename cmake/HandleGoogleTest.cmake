643497b3c48490a0a0cf535af79423b8867fb4c6
Andrew Wildman <apw4@uw.edu>
2021-04-30 21:44:10 -0700

    Add basic installation targets to CMake

--- cmake/HandleGoogleTest.cmake
+++ cmake/HandleGoogleTest.cmake
@@ -1,11 +1,15 @@
-include ( FetchContent )
+include(FetchContent)
 
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
 )
 
-FetchContent_MakeAvailable( googletest )
+FetchContent_GetProperties(googletest)
+if(NOT googletest_POPULATED)
+  FetchContent_Populate(googletest)
+  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
 
-add_library( X2Chem::gtest ALIAS gtest )
-add_library( X2Chem::gmock ALIAS gmock )
+add_library(X2Chem::gtest ALIAS gtest)
+add_library(X2Chem::gmock ALIAS gmock)
